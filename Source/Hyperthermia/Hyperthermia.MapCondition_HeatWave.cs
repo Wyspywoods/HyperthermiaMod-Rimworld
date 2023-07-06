359c48e6cf0e282bf5eb08da7f0a206bb3bedea6
Zac Peksa <138825953+Wyspywoods@users.noreply.github.com>
2023-07-06 18:19:11 -0400

    Add files via upload

---
+++ Source/Hyperthermia/Hyperthermia.MapCondition_HeatWave.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System;
+
+namespace Hyperthermia
+{
+    public class MapCondition_HeatWave : MapCondition
+    {
+        private const int LerpTicks = 12000;
+
+        private const float MaxTempOffset = 30f;
+
+        public override float TemperatureOffset()
+        {
+            return MapConditionUtility.LerpInOutValue((float)base.TicksPassed, (float)base.TicksLeft, 12000f, 30f);
+        }
+    }
+}
